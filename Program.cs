639a7e855c37444bd0064101cc57893075beba14
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2023-08-30 16:54:26 +0000

    Completed Assignement

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Xml;
 
 namespace NoviceChallenges
@@ -9,119 +10,121 @@ namespace NoviceChallenges
         {
             // Use the Main method to test your functions.
 
-            // // Add
-            // Console.WriteLine("Add ---------------------------------");
-            // Console.WriteLine(Add(1, 2)); 
-            // Console.WriteLine(Add(2, 1));
-            // Console.WriteLine(Add(0, 0));
-            // Console.WriteLine(Add(10, 10) + " \n");
+            // Add
+            Console.WriteLine("Add ---------------------------------");
+            Console.WriteLine(Add(1, 2)); 
+            Console.WriteLine(Add(2, 1));
+            Console.WriteLine(Add(0, 0));
+            Console.WriteLine(Add(10, 10) + " \n");
 
-            // //Is Even
-            // Console.WriteLine("Is Even ---------------------------------");
-            // Console.WriteLine(IsEven(1));
-            // Console.WriteLine(IsEven(2));
-            // Console.WriteLine(IsEven(0));
-            // Console.WriteLine(IsEven(10) + " \n");
+            //Is Even
+            Console.WriteLine("Is Even ---------------------------------");
+            Console.WriteLine(IsEven(1));
+            Console.WriteLine(IsEven(2));
+            Console.WriteLine(IsEven(0));
+            Console.WriteLine(IsEven(10) + " \n");
 
-            // //Max of three
-            // Console.WriteLine("Max of three ---------------------------------");
-            // Console.WriteLine(MaxOfThree(1, 2, 3));
-            // Console.WriteLine(MaxOfThree(3, 2, 1));
-            // Console.WriteLine(MaxOfThree(10, 5, 10));
-            // Console.WriteLine(MaxOfThree(10, 10, 10) + " \n");
+            //Max of three
+            Console.WriteLine("Max of three ---------------------------------");
+            Console.WriteLine(MaxOfThree(1, 2, 3));
+            Console.WriteLine(MaxOfThree(3, 2, 1));
+            Console.WriteLine(MaxOfThree(10, 5, 10));
+            Console.WriteLine(MaxOfThree(10, 10, 10) + " \n");
 
-            // //String length
-            // Console.WriteLine("String Length ---------------------------------");
-            // Console.WriteLine(StringLength("a"));
-            // Console.WriteLine(StringLength("ab"));
-            // Console.WriteLine(StringLength("abc"));
-            // Console.WriteLine(StringLength("abcdjsahdksajhkdhaksdhaksajhdkajshdkajhskdjahskjdhaksjdhkajshdkajshdkjahskdhaksdhkajsd") + " \n");
+            //String length
+            Console.WriteLine("String Length ---------------------------------");
+            Console.WriteLine(StringLength("a"));
+            Console.WriteLine(StringLength("ab"));
+            Console.WriteLine(StringLength("abc"));
+            Console.WriteLine(StringLength("abcdjsahdksajhkdhaksdhaksajhdkajshdkajhskdjahskjdhaksjdhkajshdkajshdkjahskdhaksdhkajsd") + " \n");
 
-            // //Starts hello
-            // Console.WriteLine("Starts With Hello ---------------------------------");
-            // Console.WriteLine(StartsHello("Reverse"));
-            // Console.WriteLine(StartsHello("Hello"));
-            // Console.WriteLine(StartsHello("   Hello"));
-            // Console.WriteLine(StartsHello("hello  "));
-            // Console.WriteLine(StartsHello(" hello") + " \n");
+            //Starts hello
+            Console.WriteLine("Starts With Hello ---------------------------------");
+            Console.WriteLine(StartsHello("Reverse"));
+            Console.WriteLine(StartsHello("Hello"));
+            Console.WriteLine(StartsHello("   Hello"));
+            Console.WriteLine(StartsHello("hello  "));
+            Console.WriteLine(StartsHello(" hello") + " \n");
 
-            // //Reverse string
-            // Console.WriteLine("Reverse ---------------------------------");
-            // Console.WriteLine(ReverseString("Reverse"));
-            // Console.WriteLine(ReverseString("Hello"));
-            // Console.WriteLine(ReverseString("esreveR") + " \n");
+            //Reverse string
+            Console.WriteLine("Reverse ---------------------------------");
+            Console.WriteLine(ReverseString("Reverse"));
+            Console.WriteLine(ReverseString("Hello"));
+            Console.WriteLine(ReverseString("esreveR") + " \n");
 
-            // //Factorial
-            // Console.WriteLine("factorial ---------------------------------");
-            // Console.WriteLine(Factorial(0));
-            // Console.WriteLine(Factorial(1));
-            // Console.WriteLine(Factorial(2));
-            // Console.WriteLine(Factorial(3));
-            // Console.WriteLine(Factorial(4));
-            // Console.WriteLine(Factorial(8) + " \n");
+            //Factorial
+            Console.WriteLine("factorial ---------------------------------");
+            Console.WriteLine(Factorial(0));
+            Console.WriteLine(Factorial(1));
+            Console.WriteLine(Factorial(2));
+            Console.WriteLine(Factorial(3));
+            Console.WriteLine(Factorial(4));
+            Console.WriteLine(Factorial(8) + " \n");
 
-            // Console.WriteLine("factorial but Recursive :) ---------------------------------");
-            // Console.WriteLine(FacortialButRecursive(0));
-            // Console.WriteLine(FacortialButRecursive(1));
-            // Console.WriteLine(FacortialButRecursive(2));
-            // Console.WriteLine(FacortialButRecursive(3));
-            // Console.WriteLine(FacortialButRecursive(4));
-            // Console.WriteLine(FacortialButRecursive(8) + " \n");
+            Console.WriteLine("factorial but Recursive :) ---------------------------------");
+            Console.WriteLine(FacortialButRecursive(0));
+            Console.WriteLine(FacortialButRecursive(1));
+            Console.WriteLine(FacortialButRecursive(2));
+            Console.WriteLine(FacortialButRecursive(3));
+            Console.WriteLine(FacortialButRecursive(4));
+            Console.WriteLine(FacortialButRecursive(8) + " \n");
 
-            // //Is Prime
-            // Console.WriteLine("Is Prime ---------------------------------");
-            // Console.WriteLine(IsPrime(0));
-            // Console.WriteLine(IsPrime(1));
-            // Console.WriteLine(IsPrime(2));
-            // Console.WriteLine(IsPrime(3));
-            // Console.WriteLine(IsPrime(4));
-            // Console.WriteLine(IsPrime(5) + " \n");
+            //Is Prime
+            Console.WriteLine("Is Prime ---------------------------------");
+            Console.WriteLine(IsPrime(0));
+            Console.WriteLine(IsPrime(1));
+            Console.WriteLine(IsPrime(2));
+            Console.WriteLine(IsPrime(3));
+            Console.WriteLine(IsPrime(4));
+            Console.WriteLine(IsPrime(5) + " \n");
 
-            // //Fibonacci
-            // Console.WriteLine("Fibonacci ---------------------------------");
-            // Console.WriteLine(Fibonacci(0));
-            // Console.WriteLine(Fibonacci(2));
-            // Console.WriteLine(Fibonacci(5));
-            // Console.WriteLine(Fibonacci(10));
-            // Console.WriteLine(Fibonacci(11) + " \n");
+            //Fibonacci
+            Console.WriteLine("Fibonacci ---------------------------------");
+            Console.WriteLine(Fibonacci(0));
+            Console.WriteLine(Fibonacci(2));
+            Console.WriteLine(Fibonacci(5));
+            Console.WriteLine(Fibonacci(10));
+            Console.WriteLine(Fibonacci(11) + " \n");
 
-            // //Largest in Array
-            // Console.WriteLine("Largest In Array ---------------------------------");
-            // Console.WriteLine(LargestInArray(new int[] {1, 2, 3}));
-            // Console.WriteLine(LargestInArray(new int[] {3, 2, 1}));
-            // Console.WriteLine(LargestInArray(new int[] {10, 20, 30}));
-            // Console.WriteLine(LargestInArray(new int[] {25, 93, 28, 44, 21}) + " \n");
+            //Largest in Array
+            Console.WriteLine("Largest In Array ---------------------------------");
+            Console.WriteLine(LargestInArray(new int[] {1, 2, 3}));
+            Console.WriteLine(LargestInArray(new int[] {3, 2, 1}));
+            Console.WriteLine(LargestInArray(new int[] {10, 20, 30}));
+            Console.WriteLine(LargestInArray(new int[] {25, 93, 28, 44, 21}) + " \n");
 
             //Is Palindrome
             Console.WriteLine("Is Palindrome ---------------------------------");
+            Console.WriteLine(IsPalindrome("racecar"));
+            Console.WriteLine(IsPalindrome("ooo"));
             Console.WriteLine(IsPalindrome("tacocat"));
             Console.WriteLine(IsPalindrome("TaCocAT"));
             Console.WriteLine(IsPalindrome("ooguim boga")+ " \n");
 
-            // //Array Sum
-            // Console.WriteLine("Array Sum ---------------------------------");
-            // Console.WriteLine(ArraySum(new int[] {1, 2, 3}));
-            // Console.WriteLine(ArraySum(new int[] {3, 2, 1}));
-            // Console.WriteLine(ArraySum(new int[] {10, 20, 30}));
-            // Console.WriteLine(ArraySum(new int[] {0, 0, 0}));
-            // Console.WriteLine(ArraySum(new int[] {25, 93, 28, 44, 21}) + " \n");
+            //Array Sum
+            Console.WriteLine("Array Sum ---------------------------------");
+            Console.WriteLine(ArraySum(new int[] {1, 2, 3}));
+            Console.WriteLine(ArraySum(new int[] {3, 2, 1}));
+            Console.WriteLine(ArraySum(new int[] {10, 20, 30}));
+            Console.WriteLine(ArraySum(new int[] {0, 0, 0}));
+            Console.WriteLine(ArraySum(new int[] {25, 93, 28, 44, 21}) + " \n");
 
-            // //Char Count
-            // Console.WriteLine("Char Count ---------------------------------");
-            // Console.WriteLine(CharCount("tacocat", 'c'));
-            // Console.WriteLine(CharCount("", 'z'));
-            // Console.WriteLine(CharCount("zebra", 'z'));
-            // Console.WriteLine(CharCount("ccccccccc", 'c'));
+            //Char Count
+            Console.WriteLine("Char Count ---------------------------------");
+            Console.WriteLine(CharCount("tacocat", 'c'));
+            Console.WriteLine(CharCount("", 'z'));
+            Console.WriteLine(CharCount("zebra", 'z'));
+            Console.WriteLine(CharCount("ccccccccc", 'c') + " \n");
 
-            // // Concatenate Strings
-            // Console.WriteLine("Concatenate Strings ---------------------------------");
-            // Console.WriteLine(ConcatenateStrings("tacocat", "purple"));
-            // Console.WriteLine(ConcatenateStrings("tacocat", "is green"));
-            // Console.WriteLine(ConcatenateStrings("", ""));
+            // Concatenate Strings
+            Console.WriteLine("Concatenate Strings ---------------------------------");
+            Console.WriteLine(ConcatenateStrings("tacocat", "purple"));
+            Console.WriteLine(ConcatenateStrings("tacocat", "is green"));
+            Console.WriteLine(ConcatenateStrings("", "") + " \n");
 
             // Swap Ends
             Console.WriteLine("Swap Ends ---------------------------------");
-            Console.WriteLine(SwapEnds("tacoca"));
+            Console.WriteLine(SwapEnds("hello"));
             Console.WriteLine(SwapEnds("bat"));
             Console.WriteLine(SwapEnds(""));
             Console.WriteLine(SwapEnds("oogum"));
@@ -249,23 +252,17 @@ namespace NoviceChallenges
         // 11. Check if a string is a palindrome (reads the same forward and backward).
         public static bool IsPalindrome(string s)
         {
-            string lowerString = s.ToLower();
-            int check = 0;
-            for (int i = 0; i < lowerString.Length; i++)
+            int j = s.Length;
+            int middle = s.Length / 2;
+            for (int i = 0; i < middle; i++)
             {
-                for (int j = lowerString.Length; j < 0; j--)
+                j--;
+                if (s[i] != s[j]) 
                 {
-                    if(lowerString[i] != lowerString[j])
-                        return false;
-                    else
-                        check++;
+                    return false;
                 }
-            }
-
-            if (check == s.Length)
-                return true;
-
-            return false;
+            }  
+            return true;
         }
 
         // 12. Given an array of integers, return the sum of its elements.
@@ -307,21 +304,21 @@ namespace NoviceChallenges
 
             if (trimmed.Length <= 1) return s;
 
-            char firstChar = s[0];
-            char lastChar = s[s.Length - 1];
-            char[] swappedEnds = new char[s.Length];
+            char firstChar = trimmed[0];
+            char lastChar = trimmed[trimmed.Length -1];
+            char[] swappedEnds = new char[trimmed.Length];
             swappedEnds[0] = lastChar;
 
-            for (int i = 1; i < s.Length; i++)
+            for (int i = 1; i < trimmed.Length -1; i++)
             {
                 swappedEnds[i] = s[i];
             }
 
-            swappedEnds[lastChar] = firstChar;
+            swappedEnds[swappedEnds.Length -1] = firstChar;
 
             string final = new string (swappedEnds);
 
-            return final;
+            return final; //oellh
         }
     }
 }
